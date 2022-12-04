2439974f1955435c03910ed1806849cd5b260ad0
SAID AL BLUSHI <said.alblushi@gmail.com>
2022-12-04 17:00:25 +0400

    adding multyble users

--- src/evaluationSaid/Search.java
+++ src/evaluationSaid/Search.java
@@ -6,19 +6,27 @@ import java.util.Scanner;
 
 public class Search {
 
-	public static void main(String args[]) throws FileNotFoundException {
-		// Reading the word to be found from the user
+	static void minue() {
 
-		Scanner sc = new Scanner(System.in);
+		System.out.println("************************************");
+		System.out.println(" 1- Search API information  ");
+		System.out.println(" 0 - Exit  ");
+		System.out.println("************************************");
 
-		boolean exit = false;
+	}
 
-		do {
+	static void search() throws FileNotFoundException {
+		boolean isExit = true;
+		while (isExit) {
+			Scanner sc = new Scanner(System.in);
 
 			System.out.println("**********************************");
-
+			// Reading the word to be found from the user
 			System.out.println("Enter the word you want to search ");
 			String input = sc.next();
+
+			// consuming the <enter> from input above
+			sc.nextLine();
 			boolean word2 = false;
 			int wordCount = 0;
 
@@ -26,8 +34,8 @@ public class Search {
 			Scanner sc1 = new Scanner(new FileInputStream("evaluationFile.txt"));
 			while (sc1.hasNextLine()) {
 				String word1 = sc1.nextLine();
-//         System.out.println(line);
-				if (word1.indexOf(input)!= -1) {
+//     System.out.println(line);
+				if (word1.indexOf(input) != -1) {
 					word2 = true;
 					wordCount = wordCount + 1;
 				}
@@ -42,6 +50,52 @@ public class Search {
 				System.out.println("__________________________________");
 
 			}
-		} while (exit = true);
+			System.out.println("if you want to search another word press 1 and 2 to exit ");
+			Integer s = sc.nextInt();
+			if (s == 1) {
+				isExit = true;
+			} else if (s == 2) {
+				isExit = false;
+				System.out.println("Good Bye");
+			}
+
+		}
+	}
+
+	static void exitMassege() {
+
+		System.out.println("Good Bay & See You Agin");
+
+	}
+
+	public static void main(String args[]) throws FileNotFoundException {
+		Scanner sc = new Scanner(System.in);
+
+		boolean exit = true;
+
+		while (exit) {
+
+			minue();
+			int option = sc.nextInt();
+
+			switch (option) {
+
+			case 1:
+
+				search();
+
+				break;
+
+			case 0:
+
+				exitMassege();
+				exit = false;
+
+				break;
+
+			default:
+
+			}
+		}
 	}
 }
--- src/evaluationSaid/WriteAndRead.java
+++ src/evaluationSaid/WriteAndRead.java
@@ -10,24 +10,42 @@ import java.net.http.HttpResponse;
 import java.util.Scanner;
 
 public class WriteAndRead {
-	
-	
-	
-	
-	static void onWrite() throws Exception, Throwable {
-		
+
+	static void minue() {
+
+		System.out.println("************************************");
+		System.out.println(" 1- print and save API information  ");
+		System.out.println(" 2- saveing API information  ");
+		System.out.println(" 0 - Exit  ");
+		System.out.println("************************************");
+
+	}
+
+	static void onRead() throws Exception, Throwable {
+
 		HttpClient client = HttpClient.newHttpClient();
 
 		System.out.println(" printing (API) information  ");
 
-		HttpRequest request = HttpRequest.newBuilder().uri(URI.create("https://ipinfo.io/161.185.160.39/geo"))
-				.build();
+		HttpRequest request = HttpRequest.newBuilder().uri(URI.create("https://ipinfo.io/161.185.160.39/geo")).build();
 
 		HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
 		System.out.println(response.body());
 
+	}
+
+	static void onWrite() throws Throwable, InterruptedException {
+
+		HttpClient client = HttpClient.newHttpClient();
+		HttpRequest request = HttpRequest.newBuilder().uri(URI.create("https://ipinfo.io/161.185.160.39/geo")).build();
+
 		/* write json in file */
+		HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+		System.out.println("API data saved in txt file name:'evaluationFile'");
+		System.out.println("___________________________________");
+
 		String path = "C:\\Users\\user002\\eclipse-workspace\\saidevaluation\\" + "evaluationFile.txt";
 
 		try (PrintWriter out = new PrintWriter(new FileWriter(path))) {
@@ -38,52 +56,59 @@ public class WriteAndRead {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+
+	}
+	
+
+	static void wroungOption() {
+		System.out.println("Wroung option!!!!!!!! please Enter correct option");
 	}
 
-	public static void main(String[] args) throws Exception, Throwable   {
-		
+	static void exitMassege() {
+
+		System.out.println("Good Bay & See You Agin");
+
+	}
+
+	public static void main(String[] args) throws Exception, Throwable {
+
 		Scanner sc = new Scanner(System.in);
 
 		boolean exit = true;
 
-		do {
-			System.out.println("************************************");
-			System.out.println(" 1- print and save API information  ");
-			System.out.println("************************************");
+		while (exit) {
+
+			minue();
 
 			int option = sc.nextInt();
 
 			switch (option) {
-			
-			
+
 			case 1:
 
+				onRead();
+
+				break;
+
+			case 2:
+
 				onWrite();
-				System.out.println("API data saved in txt file");
-				System.out.println("___________________________________");
 
+				break;
 
-			break;
-			   
-		 default: 
-			 
-				System.out.println("Wroung option!!!!!!!! please Enter correct option");
+			case 0:
+				exitMassege();
+				exit = false;
+
+				break;
+
+			default:
+
+				wroungOption();
+
+			}
+
+		}
 
 	}
-
-} while (exit = true);
-
-	
 }
-}
-	
-
-		
-		
-		
-			
-		
-		
-		
-		
-		
